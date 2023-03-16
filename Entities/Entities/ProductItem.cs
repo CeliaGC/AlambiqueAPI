d989caf767d0f69d71826b3e77a63ebe98ca37fa
MartaMunMol <marmunmol@gmail.com>
2023-03-16 12:33:24 +0100

    Update productsitem with new attributes

--- Entities/Entities/ProductItem.cs
+++ Entities/Entities/ProductItem.cs
@@ -16,13 +16,20 @@ namespace Entities.Entities
         }
         public int Id { get; set; }
         public string ProductName { get; set; }
-        public string ProductBrand { get; set; }
+        public string ProductCategory { get; set; }
+        public string Description { get; set; }
+        public string Ingredients { get; set; }
+        public decimal EffectsDuration { get; set; }
+        public string AdverseEffects { get; set; }
+        public byte[] Image { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
         public Guid IdWeb { get; set; }
         public DateTime InsertDate { get; set; }
         public DateTime UpdateDate { get; set; }
         public bool IsActive { get; private set; }
         public bool IsPublic { get; private set; }
-        public decimal RawPrice { get; set; }
+        
         
     }
 }
